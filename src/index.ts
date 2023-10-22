c515bca94e5b02270d67b88922c8a91a07b19522
Fityan <fityannugroho@gmail.com>
2023-10-22 11:36:31 +0700

    feat: multiple download

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,30 @@
-import { download as imgdl } from './downloader.js';
+import { DEFAULT_NAME } from './constanta.js';
+import { DownloadOptions, download } from './downloader.js';
 
-export default imgdl;
+export type Options = Omit<DownloadOptions, 'name'> & {
+  /**
+   * The name of the image file.
+   *
+   * You also can provide a function that returns the name.
+   * The function will be called with the original name, if it exists in the URL.
+   *
+   * The default value will be used if this value (or the function) returns an empty string.
+   *
+   * The default value will be the **original name** if it exists in the URL.
+   * Otherwise, it will be **'image'**.
+   *
+   * When downloading multiple images, we will add `-${index}` as the suffix starting from 1.
+   */
+  name?: string;
+};
+
+export default function imgdl(url: string | string[], options?: Options) {
+  if (Array.isArray(url)) {
+    return Promise.all(url.map((u, i) => download(u, {
+      ...options,
+      name: (ori) => `${options?.name ?? ori ?? DEFAULT_NAME}-${i + 1}`,
+    })));
+  }
+
+  return download(url, options);
+}
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,4 +1,53 @@
-import { describe } from 'vitest';
+import fs from 'node:fs';
+import { describe, expect, test } from 'vitest';
+import { DEFAULT_NAME } from '~/constanta.js';
+import imgdl from '~/index.js';
 
-describe.todo('`imgdl()`', () => {
+describe('`imgdl()`', () => {
+  test('single', async () => {
+    const url = 'https://picsum.photos/200/300.webp';
+    const expectedFilePath = `${process.cwd()}/300.webp`;
+
+    expect(await imgdl(url)).toEqual(expectedFilePath);
+    expect(fs.existsSync(expectedFilePath)).toBe(true); // Ensure the image is actually exists
+
+    // Cleanup
+    fs.unlinkSync(expectedFilePath);
+  }, { timeout: 10000 });
+
+  describe('multiple', () => {
+    const testUrls = ['https://picsum.photos/200/300.webp', 'https://picsum.photos/200/300'];
+    const expectedNames = ['300-1.webp', `${DEFAULT_NAME}-2.jpg`];
+
+    test('only array of `url`s', async () => {
+      const expectedFilePaths = expectedNames.map((n) => `${process.cwd()}/${n}`);
+
+      expect(await imgdl(testUrls)).toEqual(expectedFilePaths);
+      expectedFilePaths.forEach((filepath) => {
+        expect(fs.existsSync(filepath)).toBe(true); // Ensure the image is actually exists
+        fs.unlinkSync(filepath); // Cleanup
+      });
+    }, { timeout: 10000 });
+
+    test('with `directory` argument', async () => {
+      const directory = 'test/tmp';
+      const expectedFilePaths = expectedNames.map((n) => `${directory}/${n}`);
+
+      expect(await imgdl(testUrls, { directory })).toEqual(expectedFilePaths);
+      expectedFilePaths.forEach((filepath) => {
+        expect(fs.existsSync(filepath)).toBe(true); // Ensure the image is actually exists
+        fs.unlinkSync(filepath); // Cleanup
+      });
+    }, { timeout: 10000 });
+
+    test('with `name` argument', async () => {
+      const expectedFilePaths = ['asset-1.webp', 'asset-2.jpg'].map((n) => `${process.cwd()}/${n}`);
+
+      expect(await imgdl(testUrls, { name: 'asset' })).toEqual(expectedFilePaths);
+      expectedFilePaths.forEach((filepath) => {
+        expect(fs.existsSync(filepath)).toBe(true); // Ensure the image is actually exists
+        fs.unlinkSync(filepath); // Cleanup
+      });
+    });
+  });
 });
