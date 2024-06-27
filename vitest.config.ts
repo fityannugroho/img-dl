81ef44c9136e70da25e530d19adeabf172baf409
Fityan <63894003+fityannugroho@users.noreply.github.com>
2024-06-27 22:03:31 +0700

    chore: update vitest.config.ts to use 'forks' pool

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
+    pool: 'forks',
     root: './',
     alias: {
       '~': path.resolve(import.meta.dirname, './src'),
