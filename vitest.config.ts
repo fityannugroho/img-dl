d5722b8b384e4e1415da24f60852afc4f6d28b2d
Fityan <63894003+fityannugroho@users.noreply.github.com>
2024-07-11 14:14:08 +0700

    chore(deps): update `vitest` to v2

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    pool: 'forks',
     root: './',
     alias: {
       '~': path.resolve(import.meta.dirname, './src'),
